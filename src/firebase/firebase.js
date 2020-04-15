4d15bb6ed962d8bc4a19b1209f3e3ae91bec30ad
Ruesi <the_ruesi@zoho.com>
2020-04-15 17:58:13 +0100

    prod ready

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase';
 
-// Whoops pretty sure this shouldn't have been committed to github!!
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDHDdqehB0jIkInX2OpQWrO64a7S46TBdk",
-  authDomain: "dmvie-722fd.firebaseapp.com",
-  databaseURL: "https://dmvie-722fd.firebaseio.com",
-  projectId: "dmvie-722fd",
-  storageBucket: "dmvie-722fd.appspot.com",
-  messagingSenderId: "292978005955",
-  appId: "1:292978005955:web:95f47192d5d754802a8f76",
-  measurementId: "G-Z04LSEZSTJ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,3 +19,4 @@ firebase.initializeApp(firebaseConfig);
 const database  = firebase.database();
 
 export { firebase as default, database}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if(process.env.NODE_ENV === 'test') {
-  require('dotenv').config({path: '/src/config/env.test'})
+  require('dotenv').config({path: 'env.test'})
 }else if(process.env.NODE_ENV === 'development') {
-  require('dotenv').config({path: '/src/config/env.development'})
+  require('dotenv').config({path: '.env.development'});
 }
 
 module.exports = (env) => {
@@ -15,9 +16,20 @@ module.exports = (env) => {
 
   return {
     plugins: [
-      new MiniCssExtractPlugin({filename: 'styles.css'})
+      new MiniCssExtractPlugin({filename: 'styles.css'}),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+
+      })
     ],
-    entry: './src/index.js',
+    entry: ['babel-polyfill', './src/index.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
